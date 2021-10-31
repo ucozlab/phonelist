bd2b4806be44a3894fb0ce8d8d69ccbcc98c92b0
ucozlab <submerged311@gmail.com>
2021-10-31 20:38:15 +0200

    feature/added webpack & scss and js builds

---
+++ client/scripts/app.js
@@ -0,0 +1,5 @@
+import {MDCRipple} from '@material/ripple';
+const button = document.querySelector('.foo-button');
+console.log("button", button);
+const ripple = new MDCRipple(button);
+console.log("ripple", ripple);
--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .set('views', path.join(__dirname, 'server/views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/phones', (req, res) => {
+    return res.send([{
+      id: 1,
+      countryCode: "+38",
+      phone: "0631208277",
+      name: "Artem"
+    }])
+  })
+  // .get('/*', function (req, res) {
+  //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  // })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
