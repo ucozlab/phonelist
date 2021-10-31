8bde95bec0f5046387a097de3b8ed118fe8a6860
ucozlab <submerged311@gmail.com>
2021-10-31 22:46:21 +0200

    feature/end of ejs web material implementation (I'm tired of it, let's go to react)

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,17 @@
 import {MDCRipple} from '@material/ripple';
-const button = document.querySelector('.foo-button');
-console.log("button", button);
-const ripple = new MDCRipple(button);
-console.log("ripple", ripple);
+import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCList} from '@material/list';
+
+window.addEventListener('load', (event) => {
+  // const button = document.querySelector('.foo-button');
+  // console.log("button", button);
+  // const ripple = new MDCRipple(button);
+  // console.log("ripple", ripple);
+
+// Instantiation
+  const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+  const topAppBar = new MDCTopAppBar(topAppBarElement);
+  const list = new MDCList(document.querySelector('.mdc-list'));
+  const listItemRipples = list.listElements.map((listItemEl) => new MDCRipple(listItemEl));
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,32 @@
 const autoprefixer = require('autoprefixer');
 const path = require('path');
 
+function tryResolve_(url, sourceFilename) {
+  // Put require.resolve in a try/catch to avoid node-sass failing with cryptic libsass errors
+  // when the importer throws
+  try {
+    return require.resolve(url, {paths: [path.dirname(sourceFilename)]});
+  } catch (e) {
+    return '';
+  }
+}
+
+function tryResolveScss(url, sourceFilename) {
+  // Support omission of .scss and leading _
+  const normalizedUrl = url.endsWith('.scss') ? url : `${url}.scss`;
+  return tryResolve_(normalizedUrl, sourceFilename) ||
+    tryResolve_(path.join(path.dirname(normalizedUrl), `_${path.basename(normalizedUrl)}`),
+      sourceFilename);
+}
+
+function materialImporter(url, prev) {
+  if (url.startsWith('@material')) {
+    const resolved = tryResolveScss(url, prev);
+    return {file: resolved || url};
+  }
+  return {file: url};
+}
+
 module.exports = [{
   entry: ['./client/styles/app.scss', './client/scripts/app.js'],
   output: {
@@ -42,6 +68,7 @@ module.exports = [{
               // See https://github.com/webpack-contrib/sass-loader/issues/804
               webpackImporter: false,
               sassOptions: {
+                importer: materialImporter,
                 includePaths: ['./node_modules'],
               },
             },
